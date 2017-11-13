43231b6e9dfcaef4a1dc772bcabfb8ea5c39880f
adami0 <adam.madbak@gmail.com>
2017-11-13 10:59:12 +0100

    modif js

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+(function bizOnline () {
+
+var tableau = [];
+
+  function Product (info) {
+    this.reference = info.reference;
+    this.name = info.name;
+    this.price = info.price;
+    this.description = info.description;
+    this.image = info.image;
+    this.color = info.color;
+    this.weight = info.weight;
+  }
+
+
+
+window.onload = function () {
+
+
+
+
+
+
+}
+
+}());
