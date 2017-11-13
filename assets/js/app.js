ee930c0cdaeabf3c36a0253fece632133862cb79
Richard Phonthibsvads <phs.richards@gmail.com>
2017-11-13 12:51:37 +0100

    function pour créer un produit & une pour stocker

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 (function bizOnline () {
 
-var tableau = [];
+  "use strict";
+
+  var tableau = [];
+  var input;
+  var newProduct;
 
   function Product (info) {
     this.reference = info.reference;
@@ -12,15 +16,42 @@ var tableau = [];
     this.weight = info.weight;
   }
 
+  // var p1 = new Product ({
+  //   name: "PS4",
+  //   reference: "",
+  //   description: "console sony 1 To",
+  //   color: "white",
+  //   image: "",
+  //   price: "200",
+  //   weight: ""
+  // });
+  //
+  // console.log(p1);
+
+  function createProduct() {
+    var newProduct = new Product({
+      name: document.getElementById('name').value,
+      price: document.getElementById('price').value + "euros",
+      description: document.getElementById('description').value
+    });
+  }
+
+  function storeProduct() {
+    tableau.push(newProduct);
+  }
+
+  function validateProduct() {
+
+  }
 
 
-window.onload = function () {
+
+  window.onload = function () {
+
+    console.log(createProduct());
 
 
 
-
-
-
-}
+  }
 
 }());
