e5d1d57971bd64eea6888562e30b76a2c3dcca50
adami0 <adam.madbak@gmail.com>
2017-11-19 13:45:19 +0100

    modification JS entamée

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,22 @@
   "use strict";
 
   var tableau = [];
-  var inputNameValue, inputPriceValue, inputDescriptionValue;
   var newProduct;
   var i = 1;
   var j, k;
   var deleteBtns;
   var table_rows;
+  var modifyBtns;
+  var deleteBtns;
+  var inputNameValue;
+  var inputPriceValue;
+  var inputDescriptionValue;
+  var inputColorValue;
+  var inputWeightValue;
+  var beforeInputNameValue, beforeInputPriceValue, beforeInputDescriptionValue;
+  var test;
+
+
 
   function Product (info) {
     this.reference = info.reference;
@@ -38,26 +48,26 @@
     tableau.push(Product.name);
   }*/
 
-  function selectPriceValue(){}
-
   function createProduct() {
         newProduct = new Product({
         reference: i++,
         name: document.getElementById("name").value,
         price: document.getElementById("price").value,
         description: document.getElementById("description").value,
+        color: document.getElementById("color").value,
+        weight: document.getElementById("weight").value,
       });
     console.log(this);
     storeProduct();
     tableau.push(newProduct);
-    document.getElementById('name').value = "";
-    document.getElementById('price').value = "";
-    document.getElementById('description').value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("price").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("color").value = "";
+    document.getElementById("weight").value= "";
     /*  for (j=1; j<tableau.length; j++) {
         document.getElementById(`fa${j}`).onclick = deleteProduct;
       }*/
-    deleteBtns = document.querySelectorAll(".fa-trash");
-    table_rows = document.querySelectorAll(".table_rows");
     console.log(deleteBtns);
     console.log(newProduct);
     //document.getElementById("name").oninput = selectNameValue;
@@ -65,38 +75,75 @@
     console.log(this);
   //  console.log("--------elemparenteElement--------------");
     attachDeleteBtnsToRows();
+    attachModifyBtnsToRows();
   }
 
   function storeProduct() {
-    document.querySelector("tbody").innerHTML += `<tr id="tr${tableau.length + 1}" class="table_rows"><td>${newProduct.reference}</td><td>${newProduct.name}</td><td>${document.getElementById('price').value}</td><td>${document.getElementById('description').value}</td><td><i class="fa fa-info-circle" aria-hidden="true"></i></td><td><i id="fa${tableau.length+1}" class="fa fa-trash" aria-hidden="true"></i></td></tr>`;
-    console.log(document.querySelector("tbody"));
-;
+    document.querySelector("tbody").innerHTML += `<tr id="tr${tableau.length + 1}" class="table_rows"><td>${newProduct.reference}</td><td>${newProduct.name}</td><td>${document.getElementById('price').value}</td><td>${document.getElementById('description').value}</td><td><i class="fa fa-info-circle" aria-hidden="true"></i></td><td class="modify">cliquez ici pour modifier</td><td><i id="fa${tableau.length+1}" class="fa fa-trash" aria-hidden="true"></i></td></tr>`;
+}
+
+
+  function attachDeleteBtnsToRows() {
+    deleteBtns = document.querySelectorAll(".fa-trash");
+    console.log("-------attach--------");
+    console.log(this);
+    console.log(deleteBtns);
+    for (j=0; j<deleteBtns.length; j+=1) {
+      deleteBtns[j].onclick = deleteRows;
+    }
   }
 
   function clickCreateProduct() {
     document.getElementById("validate").onclick = createProduct;
   }
 
-  function attachDeleteBtnsToRows() {
-    console.log("-------attach--------");
-    console.log(this);
-    for (j=0; j<deleteBtns.length; j+=1) {
-      deleteBtns[j].onclick = deleteRows;
-    }
+  function deleteRows() {
+    console.log("-----deleteRows------");
+    var answer = confirm("Do you really want to delete this product? Ok to confirm");
+    console.log(answer);
+    if (answer) {
+    this.parentElement.parentElement.remove();
+    delete tableau[this.parentElement.parentElement.firstChild.textContent-1];
+    alert("The selected product was successfully removed");
+  }
+  else {alert("The selected product wasn't removed.")};
   }
 
-  function deleteRows(num) {
-    console.log("-----deleteRows------");
-    this.parentElement.parentElement.remove();
-    console.log(this.parentElement.parentElement.firstChild.textContent);
-    delete tableau[this.parentElement.parentElement.firstChild.textContent-1];
-    console.log(tableau[0]);
-    console.dir(tableau);
-    console.log("------apressuppression---------");
+  function attachModifyBtnsToRows() {
+    modifyBtns = document.querySelectorAll(".modify");
+    for(var k=0; k<modifyBtns.length; k+=1) {
+      modifyBtns[k].onclick = modifyRows;
+    }
     console.log(this);
-    console.log("----thistableau-----");
-    console.log(tableau[this]);
-    console.log(tableau);
+  }
+
+  function modifyRows() {
+    console.log("------modify-----");
+    console.log(this); // THE THIS MAUX DE TÊTES!!!!!
+    console.log(this.parentElement.childNodes[1]);
+    console.log(this.parentElement.childNodes);
+    this.parentElement.childNodes[1].innerHTML = `<input type=text placeholder=${this.parentElement.childNodes[1].textContent}>`;
+    this.parentElement.childNodes[2].innerHTML = `<input type=text placeholder=${this.parentElement.childNodes[2].textContent}>`;
+    this.parentElement.childNodes[3].innerHTML = `<input type=text placeholder=${this.parentElement.childNodes[3].textContent}>`;
+    this.parentElement.childNodes[5].innerHTML = `<td>cliquer ici afin de valider les modifications</td>`;
+    console.log(this);
+    console.log(test);
+    this.parentElement.childNodes[5].onclick = recuperateInputValues;
+  }
+
+  function recuperateInputValues() {
+    console.log("----recuperate----");
+    console.log(this.parentElement.childNodes);
+    test = this.parentElement.childNodes[1].oninput;
+    console.log(test);
+    for (var m=1; m<this.parentElement.childNodes.length-3; m+=1) {
+      if (this.parentElement.childNodes[m].textContent != this.parentElement.childNodes[m].firstChild.value) {
+        this.parentElement.childNodes[m].textContent = this.parentElement.childNodes[m].firstChild.value;
+        console.log("true");
+      } else {
+        console.log("false");
+      }
+    }
   }
 
   function deleteProduct(evt) {
