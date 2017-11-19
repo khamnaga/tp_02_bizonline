87b37c4ad1d8af8f950280871e0a5740ddd53443
Richard Phonthibsvads <phs.richards@gmail.com>
2017-11-19 16:45:30 +0100

    maj app.js avant merge

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,18 @@
   "use strict";
 
   var tableau = [];
-  var inputNameValue, inputPriceValue, inputDescriptionValue;
   var newProduct;
   var i = 1;
   var j, k;
   var deleteBtns;
-  var table_rows;
+  var modifyBtns;
+  var deleteBtns;
+
+/**
+ * [Product description]
+ * @param       {[type]} info [description]
+ * @constructor
+ */
 
   function Product (info) {
     this.reference = info.reference;
@@ -20,25 +26,10 @@
     this.weight = info.weight;
   }
 
-  // var p1 = new Product ({
-  //   name: "PS4",
-  //   reference: "",
-  //   description: "console sony 1 To",
-  //   color: "white",
-  //   image: "",
-  //   price: "200",
-  //   weight: ""
-  // });
-  //
-  // console.log(p1);
-
-/*  function selectNameValue(evt) {
-    const source1 = evt.target;
-    inputvalue = source.value;
-    tableau.push(Product.name);
-  }*/
-
-  function selectPriceValue(){}
+/**
+ * [createProduct description]
+ * @return {[type]} [description]
+ */
 
   function createProduct() {
         newProduct = new Product({
@@ -46,92 +37,118 @@
         name: document.getElementById("name").value,
         price: document.getElementById("price").value,
         description: document.getElementById("description").value,
+        color: document.getElementById("color").value,
+        weight: document.getElementById("weight").value,
       });
-    console.log(this);
     storeProduct();
     tableau.push(newProduct);
-    document.getElementById('name').value = "";
-    document.getElementById('price').value = "";
-    document.getElementById('description').value = "";
-    /*  for (j=1; j<tableau.length; j++) {
-        document.getElementById(`fa${j}`).onclick = deleteProduct;
-      }*/
-    deleteBtns = document.querySelectorAll(".fa-trash");
-    table_rows = document.querySelectorAll(".table_rows");
-    console.log(deleteBtns);
-    console.log(newProduct);
-    //document.getElementById("name").oninput = selectNameValue;
-    console.log(tableau);
-    console.log(this);
-  //  console.log("--------elemparenteElement--------------");
+    document.getElementById("name").value = "";
+    document.getElementById("price").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("color").value = "";
+    document.getElementById("weight").value= "";
     attachDeleteBtnsToRows();
   }
 
+  /**
+   * [storeProduct description]
+   * @return {[type]} [description]
+   */
+
   function storeProduct() {
     document.querySelector("tbody").innerHTML += `<tr id="tr${tableau.length + 1}" class="table_rows"><td>${newProduct.reference}</td><td>${newProduct.name}</td><td>${document.getElementById('price').value}</td><td>${document.getElementById('description').value}</td><td><i class="fa fa-info-circle" aria-hidden="true"></i></td><td><i id="fa${tableau.length+1}" class="fa fa-trash" aria-hidden="true"></i></td></tr>`;
-    console.log(document.querySelector("tbody"));
-;
-  }
+}
 
-  function clickCreateProduct() {
-    document.getElementById("validate").onclick = createProduct;
-  }
+/**
+ * [attachDeleteBtnsToRows description]
+ * @return {[type]} [description]
+ */
 
   function attachDeleteBtnsToRows() {
-    console.log("-------attach--------");
-    console.log(this);
+    deleteBtns = document.querySelectorAll(".fa-trash");
     for (j=0; j<deleteBtns.length; j+=1) {
       deleteBtns[j].onclick = deleteRows;
     }
   }
 
-  function deleteRows(num) {
-    console.log("-----deleteRows------");
-    this.parentElement.parentElement.remove();
-    console.log(this.parentElement.parentElement.firstChild.textContent);
-    delete tableau[this.parentElement.parentElement.firstChild.textContent-1];
-    console.log(tableau[0]);
-    console.dir(tableau);
-    console.log("------apressuppression---------");
-    console.log(this);
-    console.log("----thistableau-----");
-    console.log(tableau[this]);
-    console.log(tableau);
+/**
+ * [clickCreateProduct description]
+ * @return {[type]} [description]
+ */
+
+  function clickCreateProduct() {
+    document.getElementById("validate").onclick = createProduct;
   }
 
-  function deleteProduct(evt) {
-    /*for (var k=0; k<tableau.length; k++) {
-      tableau.splice(tableau[k], 1);
-      console.log(tableau[k]);
-  }*/
-  var source = evt.target;
-  console.log("-----------------log de this dans delete-------------");
-  console.log(this);
-  console.log(source.id);
-  console.log(newProduct);
-  console.log(newProduct.reference);
-/*    for (k=0; k<table_rows.length; k++) {
-      if (Product.reference) {
-        source.onclick = table_rows[k].classList.add("none-display");
-      }
-}*/
-  console.log(table_rows);
-  /*var k;
-    if (deleteBtns[k] === source) {
-      table_rows[k].classList.toggle("none-display");
-      k++;
-  }*/
-  console.log(tableau);
-  console.log(this.value);
+/**
+ * [deleteRows description]
+ * @return {[type]} [description]
+ */
+
+  function deleteRows() {
+    var answer = confirm("Do you really want to delete this product? Ok to confirm");
+    if (answer) {
+    this.parentElement.parentElement.remove();
+    delete tableau[this.parentElement.parentElement.firstChild.textContent-1];
+    alert("The selected product was successfully removed");
   }
+  else {alert("The selected product wasn't removed.")};
+  }
+
+/*  function attachModifyBtnsToRows() {
+    modifyBtns = document.querySelectorAll(".modify");
+    for(var k=0; k<modifyBtns.length; k+=1) {
+      modifyBtns[k].onclick = modifyRows;
+    }
+    console.log(this);
+    console.log(tableau[this.parentElement.parentElement.firstChild.textContent-1]);
+  }
+
+/**
+ * [modifyRows description]
+ * @return {[type]} [description]
+ */
+
+  function modifyRows() {
+    console.log("------modify-----");
+    console.log(this); // THE THIS MAUX DE TÊTES!!!!!
+    console.log(this.parentElement.childNodes[1]);
+    console.log(this.parentElement.childNodes);
+    this.parentElement.childNodes[1].innerHTML = `<input type=text placeholder=${this.parentElement.childNodes[1].textContent}>`;
+    this.parentElement.childNodes[2].innerHTML = `<input type=text placeholder=${this.parentElement.childNodes[2].textContent}>`;
+    this.parentElement.childNodes[3].innerHTML = `<input type=text placeholder=${this.parentElement.childNodes[3].textContent}>`;
+    this.parentElement.childNodes[5].innerHTML = `<td>cliquer ici afin de valider les modifications</td>`;
+    console.log(this);
+    console.log(test);
+    this.parentElement.childNodes[5].onclick = recuperateInputValues;
+  }
+
+/**
+ * [recuperateInputValues description]
+ * @return {[type]} [description]
+ */
+
+  function recuperateInputValues() {
+    console.log(this.parentElement.childNodes);
+    test = this.parentElement.childNodes[1].oninput;
+    for (var m=1; m<this.parentElement.childNodes.length-3; m+=1) {
+      if (this.parentElement.childNodes[m].textContent != this.parentElement.childNodes[m].firstChild.value) {
+        this.parentElement.childNodes[m].textContent = this.parentElement.childNodes[m].firstChild.value;
+        console.log("true");
+      } else {
+        console.log("false");
+      }
+    }
+  }
+*/
+
+/**
+ * [description]
+ * @return {[type]} [description]
+ */
 
   window.onload = function () {
     clickCreateProduct();
-    console.log(tableau);
-    console.log(document.getElementById("tr1"));
-    console.log(document.getElementById("fa1"));
-    console.log(document.getElementById("name"));
-    console.log(document.querySelector("table").rows);
-  }
+}
 
 }());
